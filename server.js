928e00edebc45ac132eb9902dfd3d36c23464d8d
Jimmy <odylic.lin@gmail.com>
2021-11-24 00:06:06 -0800

    webpack rendered

--- server.js
+++ server.js
@@ -5,10 +5,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require("dotenv").config();
 const PORT = 3000;
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Hello from test endpoint" });
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server this time" });
+});
+
+app.get("/app", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
+});
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./build/index.html"));
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
